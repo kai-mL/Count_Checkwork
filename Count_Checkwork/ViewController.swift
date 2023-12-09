f85c30feec87f759f0c333e099920f9db4f9aecb
kai-mL <kakerusuzuki09tyoby@gmail.com>
2023-12-09 20:00:15 +0900

    second commit

--- Count_Checkwork/ViewController.swift
+++ Count_Checkwork/ViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //nubmerという"Int"型の変数を設定する
     var number: Int = 0
+    
+    //プラス、マイナス、ラベルの設定
     @IBOutlet var plus: UIButton!
     @IBOutlet var minnus: UIButton!
     @IBOutlet var label: UILabel!
@@ -19,14 +22,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    // プラスボタンを押した時に数を1増やしてテキストに反映する
     @IBAction func add() {
         number = number + 1
         label.text = String(number)
     }
     
+    //マイナスボタンを押した時に数を1減らしてテキストに反映する
     @IBAction func hiku() {
         number = number - 1
         label.text = String(number)
+    }
+    
+    
+    func ifBranch() {
+        //numberが10を超えた時にラベルのテキストカラーを赤に変える
+        if number > 10 {
+            label.textColor = UIColor.red
+        //numberが-10を下回った時にラベルのテキストカラーを青に変える
+        }else if number < -10 {
+            label.textColor = UIColor.blue
+        //numberがそれ以外のt機にはテキストカラーを黒にする
+        }else {
+            label.textColor = UIColor.black
+        }
+        
     }
     
 }
